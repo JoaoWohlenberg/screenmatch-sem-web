fafae0d02768a27f5cd4fe2262a271dc4041c53e
joao_wohlenberg <j.v.w.oliveira@hotmail.com>
2026-02-15 18:31:47 -0300

    Finalizando a implementação da classe Principal, aumentando coesão e diminuindo o acoplamento

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,13 +1,18 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumoApi = new ConsumoApi();
-
+    private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=3caf2323";
 
@@ -17,5 +22,18 @@ public class Principal {
         var nomeSerie = leitura.nextLine();
         var json = consumoApi.obterDados(ENDERECO + nomeSerie.trim().replace(" ", "+") + API_KEY);
 
+        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i<=dadosSerie.totalTemporadas(); i++){
+            json =consumoApi.obterDados(ENDERECO + nomeSerie.trim().replace(" ", "+") + "&season="+ i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+
+        }
+
+        temporadas.forEach(System.out::println);
     }
 }
